b48eee4fe6c693710227bb4c79dfbb6db16dc822
Travis <travis3214@yahoo.com>
2023-08-10 21:59:56 -0400

    linked routes with index.js and started homeRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
+const blogRoutes = require('./BlogRoutes');
 const commentRoutes = require('./commentRoutes');
 
+router.use('/Blogs', blogRoutes);
+router.use('/Comments', commentRoutes);
+router.use('/Users', userRoutes);
+
+module.exports = router;
