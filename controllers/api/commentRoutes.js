d951bc417ded921bb118dc40aa42b674f7744a42
Travis <travis3214@yahoo.com>
2023-08-10 21:06:01 -0400

    added a get to get all comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,6 +3,15 @@ const { Comment } = require('../../models');
 
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+    try{
+        const commentData = Comment.findAll();
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
