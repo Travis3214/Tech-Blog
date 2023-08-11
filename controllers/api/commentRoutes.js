404c24c0fb4882cfb469b85f4b3f7b84009679df
Travis <travis3214@yahoo.com>
2023-08-10 21:11:24 -0400

    fixed local variables

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -35,7 +35,7 @@ router.post('/', withAuth, async (req, res) => {
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newComment);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -43,19 +43,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const commentData = await Comment.destroy({
+    const deleteComment = await Comment.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!commentData) {
+    if (!deleteComment) {
       res.status(404).json({ message: 'No comment found with this id!' });
       return;
     }
 
-    res.status(200).json(commentData);
+    res.status(200).json(deleteComment);
   } catch (err) {
     res.status(500).json(err);
   }
